ee14d5f80d725862c9784d0adbeb6fcbbb19d637
koushika <madhavankumaravel2004@gmail.com>
2026-08-20 12:03:34 +0530

    Change greeting from 'Hello' to 'Goodbye'

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const DEFAULT_USERS = [
         id: 2,
         name: "Admin",
         email: "ashik.p@koushika.co.in",
-        password: "Ashik@987",
+        password: "Ashik@9876",
         role: "admin"
     },
     {
